4c7d3c5f19d53fc88fa84afd38041fdad027c6b8
y-ohgi <ohgi.211@gmail.com>
2023-07-01 23:36:35 +0900

    init cdk

---
+++ lambda-url-function/test/share.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Share from '../lib/share-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/share-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Share.ShareStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
